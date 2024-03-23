64dcafe5df221019fdf5ff0bc66bebf6a5a5a02f
안호진 <hojinahn@anhojin-ui-MacBookAir.local>
2024-03-23 23:43:20 +0900

    mar 23 2024

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,51 @@
 fn main() {
-    let s = String::from("hello");
+    // 4-3.
+    // let s = String::from("hello");
 
-    takes_ownership(s);
+    // takes_ownership(s);
 
-    let x = 5;
+    // let x = 5;
 
-    makes_copy(x);
+    // makes_copy(x);
+
+    // 4-4.
+    // let s1 = gives_ownership();
+
+    // let s2 = String::from("hello");
+
+    // let s3 = takes_and_gives_back(s2);
+
+    // 4-5.
+    let s1 = String::from("hello");
+
+    let (s2, len) = calculate_length(s1);
+
+    println!("The length of '{}' is {}.", s2, len);
 }
 
-fn takes_ownership(some_string: String) {
-    println!("{}", some_string);
-}
+// 4-3.
+// fn takes_ownership(some_string: String) {
+//     println!("{}", some_string);
+// }
 
-fn makes_copy(some_integer: i32) {
-    println!("{}", some_integer);
+// fn makes_copy(some_integer: i32) {
+//     println!("{}", some_integer);
+// }
+
+// 4-4.
+// fn gives_ownership() -> String {
+//     let some_string = String::from("yours");
+
+//     some_string
+// }
+
+// fn takes_and_gives_back(a_string: String) -> String {
+//     a_string
+// }
+
+//4-5.
+fn calculate_length(s: String) -> (String, usize) {
+    let length = s.len();
+
+    (s, length)
 }
