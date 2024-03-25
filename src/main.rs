e70852ed54e331e6f95b7fc5bad1bfc393577a45
Hojin Ahn <hojinahn@waldlust.co.kr>
2024-03-25 15:17:02 +0900

    mar 25 2024

--- src/main.rs
+++ src/main.rs
@@ -16,11 +16,24 @@ fn main() {
     // let s3 = takes_and_gives_back(s2);
 
     // 4-5.
-    let s1 = String::from("hello");
+    // let s1 = String::from("hello");
 
-    let (s2, len) = calculate_length(s1);
+    // let (s2, len) = calculate_length(s1);
 
-    println!("The length of '{}' is {}.", s2, len);
+    // println!("The length of '{}' is {}.", s2, len);
+
+    // 4-6.
+    // let mut s = String::from("hello");
+
+    // change(&mut s);
+
+    // println!("{}", s);
+
+    // 4-7.
+    let s = String::from("hello");
+    let len = s.len();
+    let slice = &s[3..len];
+    let slice = &s[3..];
 }
 
 // 4-3.
@@ -44,8 +57,25 @@ fn main() {
 // }
 
 //4-5.
-fn calculate_length(s: String) -> (String, usize) {
-    let length = s.len();
+// fn calculate_length(s: String) -> (String, usize) {
+//     let length = s.len();
 
-    (s, length)
+//     (s, length)
+// }
+
+// 4-6.
+// fn change(some_string: &mut String) {
+//     some_string.push_str(", world");
+// }
+
+// 4-4.
+fn first_word(s: &String) -> &str {
+    let bytes = s.as_bytes();
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return &s[0..i];
+        }
+    }
+
+    &s[..]
 }
